e46ca49655e67b0bb34ee6086a49995e9a7870dc
Jhenrich <jhenrichroquios000@gmail.com>
2026-02-09 14:21:16 +0800

    pgsql

--- config/database.php
+++ config/database.php
@@ -113,6 +113,15 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'postgres' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+        ],
+
     ],
 
     /*
